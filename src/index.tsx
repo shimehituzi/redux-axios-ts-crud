bb5c804c48a65b8da4156efc36c145100e90fe3c
shimehituzi <shimehituzi@gmail.com>
2020-02-13 18:19:18 +0900

    fireStore CR

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,49 @@
 import React, { Fragment, useState } from 'react'
 import { render } from 'react-dom'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
-type Sample = {
-  title: string
-  description: string
+const firebaseConfig = {
+  apiKey: "AIzaSyDIQeQwre7E4cUBkb--1G_jdxf1XWIpKoU",
+  authDomain: "shimehituzi-practice-d2c7c.firebaseapp.com",
+  databaseURL: "https://shimehituzi-practice-d2c7c.firebaseio.com",
+  projectId: "shimehituzi-practice-d2c7c",
+  storageBucket: "shimehituzi-practice-d2c7c.appspot.com",
+  messagingSenderId: "116370812428",
+  appId: "1:116370812428:web:d582bf886b678ebafe8829"
 }
 
-type Samples = Array<Sample>
+firebase.initializeApp(firebaseConfig)
 
-const initialFormInput: Sample = {
-  title: '',
-  description: ''
-}
+const db = firebase.firestore()
 
 const App: React.FC = () => {
+  type Sample = {
+    title: string
+    description: string
+  }
+
+  type Samples = (Array<Sample>)
+
+  const initialFormInput: Sample = {
+    title: '',
+    description: ''
+  }
+
   const [state, setState] = useState<Samples>([]) 
   const [formInput, setFormInput] = useState<Sample>(initialFormInput)
 
+  const getState = () => {
+    db.collection('samples')
+      .get().then((response) => {
+        const docs = response.docs.map(doc => doc.data() as Sample)
+        setState(docs)
+      })
+  }
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    db.collection('samples').doc().set(formInput)
+    getState()
     setFormInput({
       ...formInput,
       [e.target.id]: e.target.value
@@ -47,6 +73,7 @@ const App: React.FC = () => {
 
   return (
     <Fragment>
+      <button onClick={getState}>Get State</button>
       <input id="title" value={formInput.title} onChange={handleChange}/>
       <input id="description"value={formInput.description} onChange={handleChange}/>
       <button onClick={handleSubmit}>Create</button>
