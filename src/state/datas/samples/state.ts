446396bd283f54d5d118dd6dd4c179b15a66857b
shimehituzi <shimehituzi@gmail.com>
2020-02-19 01:23:45 +0900

    ファイル分割できた

---
+++ src/state/datas/samples/state.ts
@@ -0,0 +1,11 @@
+type Sample = {
+  id: number
+  title: string
+  description: string
+}
+
+type State = (Array<Sample>)
+
+export const initialState: State = []
+
+export default State
---
+++ src/views/state.ts
@@ -0,0 +1,3 @@
+import { RootState } from '../state/datas'
+
+export type State = RootState
