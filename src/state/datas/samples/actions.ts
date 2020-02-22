c41e0b666ca487e21d4e5122737b64756d58bf9d
shimehituzi <shimehituzi@gmail.com>
2020-02-22 16:37:00 +0900

    delete

--- src/state/datas/samples/actions.ts
+++ src/state/datas/samples/actions.ts
@@ -6,7 +6,8 @@ const actionCreator = actionCreatorFactory()
 const actions = {
   getSamples: actionCreator.async<{}, State['data']>('GET_SAMPLES'),
   setForm: actionCreator<State['form']>('SET_FORM'),
-  createSample: actionCreator.async<State['form'], State['form']>('CREATE_SAMPLES')
+  createSample: actionCreator.async<State['form'], State['form']>('CREATE_SAMPLES'),
+  destroySample: actionCreator.async<{}, State['data']>('DESTROY_SAMPLE'),
 }
 
 export default actions
